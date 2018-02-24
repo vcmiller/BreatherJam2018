00505a1e185e9acc481497ed666883f42936b4ac
Sean Briggs <seanrgbriggs@gmail.com>
2018-02-23 19:31:02 -0500

    Surprisingly okay boogers

---
+++ Assets/Scripts/Booger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Booger : MonoBehaviour {
+
+    MeshDeformer hyde;
+    Dictionary<string, Collision> cols;
+
+    private void Start()
+    {
+        cols = new Dictionary<string, Collision>();
+        hyde = GetComponent<MeshDeformer>();
+        GetComponent<Rigidbody>().AddForce(Vector3.one * 10, ForceMode.VelocityChange);
+    }
+
+    private void Update()
+    { 
+            foreach (Collision collision in cols.Values)
+            {
+                foreach (ContactPoint point in collision.contacts)
+                {
+                    hyde.AddDeformingForce(point.point, 5);
+                }
+            }
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        cols.Add(collision.gameObject.name, collision);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        cols.Remove(collision.gameObject.name);
+    }
+}
