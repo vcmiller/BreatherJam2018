89655f0e72a4e2301d96f244981532077a9c4f36
Sean Briggs <seanrgbriggs@gmail.com>
2018-02-24 15:01:26 -0500

    its ya boi

---
+++ Sniffe/Assets/Embiggen.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Embiggen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.localScale = Vector3.right;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localScale = Vector3.MoveTowards(transform.localScale, Vector3.one / 2, 5 * Time.deltaTime);
+	}
+}
